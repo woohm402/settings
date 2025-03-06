a978c7f94e12bfa3f2a6b2b300ffabff06e8bab5
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2025-03-06 23:09:26 +0900

    ✨ implement init.sh (#52)

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,1 +1,7 @@
 echo "Starting"
+curl https://raw.githubusercontent.com/woohm402/settings/refs/heads/main/settings/rio/config.toml > ~/.config/rio/config.toml
+curl https://raw.githubusercontent.com/woohm402/settings/refs/heads/main/settings/zed/settings.json > ~/.config/zed/settings.json
+curl https://raw.githubusercontent.com/woohm402/settings/refs/heads/main/settings/zed/keymap.json > ~/.config/zed/keymap.json
+curl https://raw.githubusercontent.com/woohm402/settings/refs/heads/main/settings/zed/snippets/tsx.json > ~/.config/zed/snippets/tsx.json
+curl https://raw.githubusercontent.com/woohm402/settings/refs/heads/main/settings/zsh/.zshrc > ~/.zshrc
+echo "Done"

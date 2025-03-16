f1373c13baa1b023df5ef62b940fd8a20b379eac
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2025-03-16 20:15:38 +0900

    🩹 .zshrc 덮어씌워지는 이슈 픽스 (#54)

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,11 @@
 echo "Starting"
 
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+curl -fsSL https://bun.sh/install | bash
+
 mkdir -p ~/.config/rio
 mkdir -p ~/.config/zed
 mkdir -p ~/.config/zed/snippets
@@ -8,11 +14,5 @@ curl https://raw.githubusercontent.com/woohm402/settings/refs/heads/main/setting
 curl https://raw.githubusercontent.com/woohm402/settings/refs/heads/main/settings/zed/keymap.json > ~/.config/zed/keymap.json
 curl https://raw.githubusercontent.com/woohm402/settings/refs/heads/main/settings/zed/snippets/tsx.json > ~/.config/zed/snippets/tsx.json
 curl https://raw.githubusercontent.com/woohm402/settings/refs/heads/main/settings/zsh/.zshrc > ~/.zshrc
-
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
-
-curl -fsSL https://bun.sh/install | bash
 
 echo "Done"
